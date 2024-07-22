554aa88a9754b92f1e01d7485a89526137a74f84
Pavanmuthyam123 <124612204+Pavanmuthyam123@users.noreply.github.com>
2024-07-22 21:27:29 +0530

    Add files via upload

---
+++ Spring-Boot_App/src/main/java/com/prog/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.prog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.prog.entity.UserDtls;
+import com.prog.repository.UserRepository;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class UserController {
+
+	@Autowired
+	
+	private UserRepository repo;
+	@GetMapping("/")
+	public String home()
+	{
+		return "something";
+	}
+	@PostMapping("/something")
+	public String register(@ModelAttribute UserDtls u,HttpSession session)
+	{
+		System.out.println(u);
+		repo.save(u);
+		session.setAttribute("Message","User Register Sucessfully");
+		return "something";
+	}
+}
+
